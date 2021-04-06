fbb577bdb3651cd7f87d63b8ff7a4fb7c34f270f
Antonio <atcarter005@gmail.com>
2021-04-05 22:28:43 -0500

    commit tree a2926021f977f02d5c7d0393b864355648b2551f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,36 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by_username(params[:username])
+    if @user
+      session[:user_id] = @user.id
+      @session = session
+      redirect to "/account"
+    else
+      erb :error 
+    end
 
   end
 
   get '/account' do
-
+    # @user = User.find_by_id(@session[:user_id])
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user
+    end
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
+
+
+
+
+
+
+
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+   
+    def self.current_user(hash)
+        user = User.find_by_id(hash[:user_id])
+        if user
+            user
+        end
+    end
+
+    def self.is_logged_in?(hash)
+        !!self.current_user(hash)
+    end
 end
